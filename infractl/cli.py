655dd263848534daf8464132f61099b719064f9b
Francois-Prime <107075756+Francois-Prime@users.noreply.github.com>
2025-12-16 21:07:44 -0600

    feat: adding in terraform plan command with env validation and confirmation

--- infractl/cli.py
+++ infractl/cli.py
@@ -1,21 +1,65 @@
+from random import choice
+
 import click
-
-
+import pathlib
+import subprocess
+import time
+from pathlib import Path
 
 @click.group()
 def main():
     """Infractl CLI"""
     pass
+@click.option('--env',
+              required=True,
+              type=click.Choice(['dev','qa','prod']),
+              help="Load env")
 
 @main.command()
-def plan():
+def plan(env):
     """Print Plan"""
-    click.echo("Planning changes")
+    infra_path = Path("infra")/ env
+    ## verify = str(input(f"Executing 'terraform plan' in {infra_path}: do you want to continue? -- Type 'Y' or 'N'\n"))
+
+    if not infra_path.exists() or not infra_path.is_dir():
+        click.echo(
+            message='Error: Folder path does not exist: {infra_path}',
+            err=True,
+        )
+        raise click.Abort()
+
+    click.confirm(
+        f"Executing 'terraform plan' in {infra_path}: Continue?",
+        abort=True
+    )
+
+    click.echo(
+        f'Planning infrastructure for env: {env}',
+    )
+
+    time.sleep(2)
+
+    ## if verify == 'Y':
+    result = subprocess.run(
+        ["terraform", "plan"],
+        cwd=infra_path,
+    )
+
+    if result != 0:
+        raise click.Abort()
+## else:
+       ## raise click.Abort()
 
 @main.command()
 def stage():
     """Print Stage"""
     click.echo("Staging Changes")
+
+@main.command()
+def init():
+    """Initialize plan"""
+    click.echo("Init Changes")
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
